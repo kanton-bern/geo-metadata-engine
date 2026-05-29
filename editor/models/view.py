7e2d161527297b42edf239f46c35c4be9694d153
nninnja <nbonassi@gmx.ch>
2026-05-29 11:46:41 +0200

    View: FK Dienst zu M2M

--- editor/models/view.py
+++ editor/models/view.py
@@ -3,11 +3,11 @@ from django.db import models
 class View(models.Model):
     name_view = models.CharField(max_length=255)
     beschreibung_de = models.TextField()
-    dienst = models.ForeignKey("Dienst", on_delete=models.CASCADE, null=True, blank=True)
-
+    dienste = models.ManyToManyField("Dienst", related_name="views", blank=True)
+ 
     def __str__(self):
         return self.name_view
-    
+ 
     class Meta:
         verbose_name = "View"
         verbose_name_plural = "Views"
